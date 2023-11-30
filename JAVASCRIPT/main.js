60c9dc7ddb6857c55e78845948d183f8e23c23cb
Alibasic Emrah <122388238+Emrahalibasic1@users.noreply.github.com>
2023-11-30 09:15:43 +0100

    i_toggle_01_32_12

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,6 +1,25 @@
 // faq 
-document.addEventListener('DOMContentLoaded', () => {
 
-    const faqContainer = document.querySelector('.faq-content');
+document.addEventListener('DOMContentLoaded', () => {
     
+    const faqContainer = document.querySelector('.faq-content');
+     
+    faqContainer.addEventListener('click' ,(e) =>
+    {
+       const groupHeader = e.target.closest('.faq-group-header');
+      
+      
+       if (!groupHeader) return;
+
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector('.faq-group-body');
+    const icon = groupHeader.querySelector('i');
+
+
+    // toggle icon
+    icon.classList.toggle('fa-plus');
+    icon.classList.toggle('fa-minus');
+    
+    });
 });
