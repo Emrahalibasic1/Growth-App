d52d984c8d72dfe8e18244a1f5193f96afcf9c19
Emrahalibasic1 <alibasicemrah98@gmail.com>
2024-01-11 16:43:13 +0100

    added popup modal and switch pricing footer

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -46,3 +46,56 @@ document.addEventListener('DOMContentLoaded', () => {
       mobileMenu.classList.toggle('active')
     );
   });
+
+  const sort = (li) =>{
+      let catagry = li.getAttribute('data-category');
+      let portofolioItems = document.querySelectorAll('.faq-group');
+      portofolioItems.forEach((item) => {
+          item.style.display = 'none';
+      });
+      if(catagry === 'All'){
+        portofolioItems.forEach((item) => {
+          item.style.display = 'block';
+      });
+      }
+
+      portofolioItems.forEach((item) => {
+          if(item.getAttribute('data-category').includes(catagry)){
+            item.style.display = 'block';
+          }
+      });
+  }
+
+const openModal = () => {
+  let modalWindow = document.querySelector('.popup-modal');
+  let overlay = document.querySelector('.overlay');
+  modalWindow.style.display = 'block';
+  overlay.style.display = 'block';
+}
+
+const closeModal = () => {
+  let modalWindow = document.querySelector('.popup-modal');
+  let overlay = document.querySelector('.overlay');
+  modalWindow.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
+
+/*
+added switch function with button 
+*/
+document.addEventListener('DOMContentLoaded',  () => {
+  var listItems = document.querySelectorAll('.faq-menu li');
+
+  listItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      // Uklanjanje aktivne klase sa svih elemenata
+      listItems.forEach(function (li) {
+        li.classList.remove('active');
+      });
+
+      // Postavljanje aktivne klase na kliknuti element
+      this.classList.add('active');
+    });
+  });
+});
